34fba56bccfe85074520b2d6174fdbcd6cf574a5
broken1arrow <16368286+broken1arrow@users.noreply.github.com>
2023-08-14 22:52:14 +0200

    improve logging more
    Will show a list.

--- Database/src/main/java/org/broken/arrow/database/library/Database.java
+++ Database/src/main/java/org/broken/arrow/database/library/Database.java
@@ -783,11 +783,10 @@ public abstract class Database {
 					+ (processedCount > 50_000 ? "10-20 MINUTES" : "5-10 MINUTES") + " - If server will print a crash report, ignore it, update will proceed.");
 		}
 		for (SqlCommandComposer sql : batchList) {
-			Object value = "";
+
 			try (PreparedStatement statement = connection.prepareStatement(sql.getPreparedSQLBatch(), resultSetType, resultSetConcurrency)) {
 				// Populate the updates map with data where the outer key is the row identifier (id)
 				for (Entry<Integer, Object> column : sql.getColumns().entrySet()) {
-					value = column.getValue();
 					statement.setObject(column.getKey(), column.getValue());
 				}
 				// Adding the current set of parameters to the batch
@@ -796,7 +795,7 @@ public abstract class Database {
 			} catch (SQLException e) {
 				// Handle the exception for a specific statement
 				LogMsg.warn("Could not execute the prepared batch. \"" + sql.getPreparedSQLBatch() + "\"");
-				LogMsg.warn("Last value executed, before the error. '" + (value != null && value.toString().isEmpty() ? "No value set" : value) + "'", e);
+				LogMsg.warn("Values that should be executed. '" + sql.getColumns().values() + "'", e);
 			} finally {
 				batchUpdateGoingOn = false;
 			}
