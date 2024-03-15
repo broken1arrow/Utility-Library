2ade62a7b45e08948f5625b9f0421e74b5ebe341
broken1arrow <broken2arrow1@gmail.com>
2024-03-15 09:43:06 +0100

    improve the text for deprecated in DataSerializer

--- Serialize Utility/src/main/java/org/broken/arrow/serialize/library/DataSerializer.java
+++ Serialize Utility/src/main/java/org/broken/arrow/serialize/library/DataSerializer.java
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
  * booleans, items, memory sections, patterns, and objects implementing the ConfigurationSerializable interface.
  * The class also handles serialization of Bukkit-specific types and custom serializable objects.
  * Note: If an object does not have a known serialization method, a SerializeFailedException will be thrown.
- * @deprecated will be replaced with {@link SerializeUtility}
+ * @deprecated recommend use this class {@link SerializeUtility} this will only be used internally.
  */
 @Deprecated
 public final class DataSerializer {
