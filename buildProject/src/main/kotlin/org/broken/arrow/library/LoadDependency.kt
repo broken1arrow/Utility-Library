9df047209865d5d0bf3403a67df2ae6a18971a9f
broken1arrow <broken2arrow1@gmail.com>
2024-06-07 09:11:11 +0200

    removed unused code.

--- buildProject/src/main/kotlin/org/broken/arrow/library/LoadDependency.kt
+++ buildProject/src/main/kotlin/org/broken/arrow/library/LoadDependency.kt
@@ -15,8 +15,6 @@ class LoadDependency : Plugin<Project> {
     override fun apply(project: Project) {
         project.plugins.apply(JavaPlugin::class.java)
         configureRepositories(project)
-        //configureDependencies(project)
-        //configureTasks(project)
     }
 
     private fun configureRepositories(project: Project) {
@@ -31,30 +29,6 @@ class LoadDependency : Plugin<Project> {
             maven { url = URI("https://oss.sonatype.org/content/repositories/snapshots/") }
             maven { url = URI("https://repo.maven.apache.org/maven2/") }
             maven { url = URI("https://libraries.minecraft.net/") }
-        }
-    }
-
-    private fun configureDependencies(project: Project) {
-        project.dependencies {
-            "compileOnly"("de.tr7zw:item-nbt-api:2.12.4")
-            "compileOnly"("com.google.code.findbugs:jsr305:3.0.2")
-            "compileOnly"("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
-        }
-    }
-
-    private fun configureTasks(project: Project) {
-        project.tasks.apply {
-            withType<org.gradle.api.tasks.javadoc.Javadoc> {
-                options.encoding = Charsets.UTF_8.name()
-            }
-            withType<JavaCompile> {
-                options.encoding = Charsets.UTF_8.name()
-                sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-                targetCompatibility = JavaVersion.VERSION_1_8.toString()
-            }
-            withType<Jar> {
-                archiveFileName.set("${project.name}-${project.version}_javadoc.jar")
-            }
         }
     }
 }
