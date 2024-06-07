43f1421569176db85af241d0eb0c91d95188822c
broken1arrow <broken2arrow1@gmail.com>
2024-06-07 09:03:08 +0200

    Added some javadocs
    Added javadocs to explain the PotionsUtility in Item creator class.

--- Item Creator/src/main/java/org/broken/arrow/itemcreator/library/utility/PotionsUtility.java
+++ Item Creator/src/main/java/org/broken/arrow/itemcreator/library/utility/PotionsUtility.java
@@ -7,14 +7,24 @@ import org.bukkit.potion.PotionType;
 
 import javax.annotation.Nonnull;
 
+/**
+ * This class handle the potions. Due to 1.20.5+ does not have Potion and
+ * PotionData, this class will handle the errors if it could not set the
+ * potion type.
+ */
 public class PotionsUtility {
-    private  static Logging logger = new Logging(PotionsUtility.class);
+    private final Logging logger = new Logging(PotionsUtility.class);
     PotionMeta potionMeta;
 
     public PotionsUtility(@Nonnull PotionMeta potionMeta) {
         this.potionMeta = potionMeta;
     }
 
+    /**
+     * Set the portions to the itemstacks metadata.
+     *
+     * @param potion the portion type you want to set for the item.
+     */
     public void setPotion(PotionType potion) {
         try {
             potionMeta.setBasePotionType(potion);
