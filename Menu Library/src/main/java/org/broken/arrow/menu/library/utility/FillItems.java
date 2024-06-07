4c00234e15b690cae5b971ea7781894a7fd94d5e
broken1arrow <broken2arrow1@gmail.com>
2024-06-07 08:35:55 +0200

    Added javaDocs to the FillItems class
    Also added method to clear the list and just add one element/fill item to the list as option. Added javadocs to make it more clear how this class and methods works.

--- Menu Library/src/main/java/org/broken/arrow/menu/library/utility/FillItems.java
+++ Menu Library/src/main/java/org/broken/arrow/menu/library/utility/FillItems.java
@@ -4,14 +4,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class handles the logic around managing fill items.
+ *
+ * @param <T> The generic type of the fill items added to the list.
+ */
 public class FillItems<T> {
+    private List<T> fillItemsList = new ArrayList<>();
 
-    List<T> fillItemsList = new ArrayList<>();
-
+    /**
+     * Retrieves an unmodifiable view of the fill items list.
+     *
+     * @return an unmodifiable list of fill items.
+     */
     public List<T> getFillItems() {
         return Collections.unmodifiableList(fillItemsList);
     }
 
+    /**
+     * Retrieves the fill item at the specified index. It ensures that the index is within bounds.
+     *
+     * @param index the index of the fill item to retrieve.
+     * @return the fill item at the specified index, or null if the index is out of bounds or the list is empty.
+     */
     public T getFillItem(int index) {
         List<T> fillItems = this.getFillItems();
         if (fillItems.isEmpty())
@@ -23,20 +38,41 @@ public class FillItems<T> {
         return fillItems.get(index);
     }
 
-    public void remove(T item) {
-        fillItemsList.remove(item);
-    }
-
-    public void clear() {
-        fillItemsList.clear();
-    }
-
     /**
-     * Set the items you want connect to a slot in the menu.
+     * Sets the fill items for this list. This will replace any existing items.
+     * This will utilize the order in the list when it populates the menu, so
+     * the order matters and it also supports null values.
      *
-     * @param fillItems list of items.
+     * @param fillItems the list of fill items to set.
      */
     public void setFillItems(List<T> fillItems) {
         this.fillItemsList = fillItems;
     }
+
+    /**
+     * Adds a fill item to the list. This method will add the
+     * item at the end of the current list.
+     *
+     * @param fillItem the fill item to add.
+     */
+    public void addFillItem(T fillItem) {
+        this.fillItemsList.add(fillItem);
+    }
+
+    /**
+     * Removes the specified fill item from the list.
+     *
+     * @param item the fill item to remove.
+     */
+    public void remove(T item) {
+        fillItemsList.remove(item);
+    }
+
+    /**
+     * Clears all fill items from the list.
+     */
+    public void clear() {
+        fillItemsList.clear();
+    }
+
 }
