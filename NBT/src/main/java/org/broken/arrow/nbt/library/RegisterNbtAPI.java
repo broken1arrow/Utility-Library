b064aef97dbf400a440b918df5a7dfb41b3ab560
broken1arrow <broken2arrow1@gmail.com>
2024-08-18 15:22:06 +0200

    Added option to serialize item stacks
    Should be more stable than old way.

--- NBT/src/main/java/org/broken/arrow/nbt/library/RegisterNbtAPI.java
+++ NBT/src/main/java/org/broken/arrow/nbt/library/RegisterNbtAPI.java
@@ -1,9 +1,17 @@
 package org.broken.arrow.nbt.library;
 
 
+import de.tr7zw.changeme.nbtapi.NBT;
+import de.tr7zw.changeme.nbtapi.NBTContainer;
 import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,55 +20,78 @@ import static de.tr7zw.changeme.nbtapi.utils.MinecraftVersion.getVersion;
 
 public class RegisterNbtAPI {
 
-	private final CompMetadata compMetadata;
-	private static boolean hasScoreboardTags = true;
-	public RegisterNbtAPI(Plugin plugin, boolean turnOffLogger) {
-		Logger logger = Logger.getLogger("NBTAPI");
-		if (turnOffLogger)
-			logger.setLevel(Level.WARNING);
-		getVersion();
-		compMetadata = new CompMetadata(plugin);
-		checkClassesExist();
-	}
+    private final CompMetadata compMetadata;
+    private static boolean hasScoreboardTags = true;
 
-	public static void checkClassesExist() {
-		try {
-			Entity.class.getMethod("getScoreboardTags");
-		} catch (NoSuchMethodException | SecurityException ignore) {
-			hasScoreboardTags = false;
-		}
-	}
+    public RegisterNbtAPI(Plugin plugin, boolean turnOffLogger) {
+        Logger logger = Logger.getLogger("NBTAPI");
+        if (turnOffLogger)
+            logger.setLevel(Level.WARNING);
+        getVersion();
+        compMetadata = new CompMetadata(plugin);
+        checkClassesExist();
+    }
 
-	public static boolean isHasScoreboardTags() {
-		return hasScoreboardTags;
-	}
+    private static void checkClassesExist() {
+        try {
+            Entity.class.getMethod("getScoreboardTags");
+        } catch (NoSuchMethodException | SecurityException ignore) {
+            hasScoreboardTags = false;
+        }
+    }
 
-	/**
-	 * work in progress. Will later fix this so you can save data (is optional
-	 * method to tr7zw file saving)
-	 */
-	public void yamlLoad() {
-		// work in progress.
-	}
+    @Nonnull
+    public static byte[] serializeItemStack(final ItemStack[] itemStacks) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            NBT.itemStackArrayToNBT((itemStacks == null ? new ItemStack[0] : itemStacks)).writeCompound(outputStream);
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * Get methods to easy set metadata. If you want to set up self you can start
-	 * with this classes {@link de.tr7zw.changeme.nbtapi.NBTItem} and
-	 * {@link de.tr7zw.changeme.nbtapi.NBTEntity}
-	 * <p>&nbsp;</p>
-	 * <p>
-	 * Note: Should use these methods, give you better performance, if you don't use my methods.
-	 * </p>
-	 * <p>
-	 * {@link de.tr7zw.changeme.nbtapi.NBT#get(org.bukkit.inventory.ItemStack, java.util.function.Function)} and {@link de.tr7zw.changeme.nbtapi.NBT#get(org.bukkit.entity.Entity, java.util.function.Function)}
-	 * </p>
-	 * <p>
-	 * {@link de.tr7zw.changeme.nbtapi.NBT#modify(org.bukkit.entity.Entity, java.util.function.Function)} and {@link de.tr7zw.changeme.nbtapi.NBT#modify(org.bukkit.inventory.ItemStack, java.util.function.Function)}
-	 * </p>
-	 * @return CompMetadata class.
-	 */
-	public CompMetadata getCompMetadata() {
-		return compMetadata;
-	}
+    @Nullable
+    public static ItemStack[] deserializeItemStack(final byte[] itemStacks) {
+        if (itemStacks == null) return new ItemStack[0];
+
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(itemStacks)) {
+            return NBT.itemStackArrayFromNBT(new NBTContainer(byteArrayInputStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean isHasScoreboardTags() {
+        return hasScoreboardTags;
+    }
+
+    /**
+     * work in progress. Will later fix this so you can save data (is optional
+     * method to tr7zw file saving)
+     */
+    public void yamlLoad() {
+        // work in progress.
+    }
+
+    /**
+     * Get methods to easy set metadata. If you want to set up self you can start
+     * with this classes {@link de.tr7zw.changeme.nbtapi.NBTItem} and
+     * {@link de.tr7zw.changeme.nbtapi.NBTEntity}
+     * <p>&nbsp;</p>
+     * <p>
+     * Note: Should use these methods, give you better performance, if you don't use my methods.
+     * </p>
+     * <p>
+     * {@link de.tr7zw.changeme.nbtapi.NBT#get(org.bukkit.inventory.ItemStack, java.util.function.Function)} and {@link de.tr7zw.changeme.nbtapi.NBT#get(org.bukkit.entity.Entity, java.util.function.Function)}
+     * </p>
+     * <p>
+     * {@link de.tr7zw.changeme.nbtapi.NBT#modify(org.bukkit.entity.Entity, java.util.function.Function)} and {@link de.tr7zw.changeme.nbtapi.NBT#modify(org.bukkit.inventory.ItemStack, java.util.function.Function)}
+     * </p>
+     *
+     * @return CompMetadata class.
+     */
+    public CompMetadata getCompMetadata() {
+        return compMetadata;
+    }
 
 }
