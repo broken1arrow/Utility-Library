67ca7e6a81b3bf6d0dba4278344e094ba3f09e7c
broken1arrow <broken2arrow1@gmail.com>
2025-04-27 17:54:08 +0200

    corrected the spacing

--- Database/src/main/java/org/broken/arrow/database/library/construct/query/builder/JoinBuilder.java
+++ Database/src/main/java/org/broken/arrow/database/library/construct/query/builder/JoinBuilder.java
@@ -9,29 +9,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class JoinBuilder {
-  private final List<JoinCondition> joins = new ArrayList<>();
+    private final List<JoinCondition> joins = new ArrayList<>();
 
-  public JoinBuilder(QueryBuilder queryBuilder) {
+    public JoinBuilder(QueryBuilder queryBuilder) {
 
-  }
+    }
 
     public void join(JoinType type, String table, String alias, String onCondition) {
-    joins.add(new JoinCondition(type, table, alias, onCondition, false));
-  }
+        joins.add(new JoinCondition(type, table, alias, onCondition, false));
+    }
 
-  public void oldJoin(String table) {
-    this.oldJoin(table, null);
-  }
+    public void oldJoin(String table) {
+        this.oldJoin(table, null);
+    }
 
-  public void oldJoin(String table, String alias) {
-    joins.add(new JoinCondition(null, table, alias, null, true));
-  }
+    public void oldJoin(String table, String alias) {
+        joins.add(new JoinCondition(null, table, alias, null, true));
+    }
 
-  public String build() {
-    return joins.isEmpty() ? "" : joins.stream().map(JoinCondition::toString).collect(Collectors.joining(" "));
-  }
+    public String build() {
+        return joins.isEmpty() ? "" : joins.stream().map(JoinCondition::toString).collect(Collectors.joining(" "));
+    }
 
-  public boolean hasOldJoins() {
-    return joins.stream().anyMatch(JoinCondition::isOldStyle);
-  }
+    public boolean hasOldJoins() {
+        return joins.stream().anyMatch(JoinCondition::isOldStyle);
+    }
 }
