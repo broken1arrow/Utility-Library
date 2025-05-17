a9af69c7929660175f3b6d0147d15cd70fa940b2
broken1arrow <broken2arrow1@gmail.com>
2025-05-17 19:15:06 +0200

    fix so the list is private

--- Database/src/main/java/org/broken/arrow/database/library/construct/query/builder/tablebuilder/AlterTable.java
+++ Database/src/main/java/org/broken/arrow/database/library/construct/query/builder/tablebuilder/AlterTable.java
@@ -11,7 +11,7 @@ import java.util.StringJoiner;
 
 public class AlterTable {
 
-    public List<String> columns = new ArrayList<>();
+    private final List<String> columns = new ArrayList<>();
 
 
     public AlterTable(@Nonnull final QueryBuilder queryBuilder) {
