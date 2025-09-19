a11f1b8bd7916eadcbb35766005b9e319b053ade
broken1arrow <broken2arrow1@gmail.com>
2025-09-20 00:58:49 +0200

    added new class to safely handle null

---
+++ Item Creator/src/main/java/org/broken/arrow/library/itemcreator/serialization/typeadapter/BaseTypeAdapter.java
@@ -0,0 +1,24 @@
+package org.broken.arrow.library.itemcreator.serialization.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public abstract class BaseTypeAdapter<T> extends TypeAdapter<T> {
+
+    @Override
+    public final void write(JsonWriter out, T value) throws IOException {
+        if (value == null) {
+            out.nullValue(); // central null handling
+            return;
+        }
+        checkedWrite(out, value);
+    }
+
+    /**
+     * Adapter implementations override this method instead of write().
+     * The null check is already handled.
+     */
+    protected abstract void checkedWrite(JsonWriter out, T value) throws IOException;
+}
