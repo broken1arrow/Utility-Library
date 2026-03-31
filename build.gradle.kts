051aee8b51b1ee90d83987f651e4678a323d3375
broken1arrow <broken2arrow1@gmail.com>
2026-03-31 17:42:36 +0200

    update the version

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ subprojects {
                     artifactId = project.name
                     groupId = project.group.toString()
                     artifactId = project.name
-                    version = "0.130"
+                    version = "0.131"
                     pom {
                         name.set(project.name)
                         description.set("Description for ${project.name}")
