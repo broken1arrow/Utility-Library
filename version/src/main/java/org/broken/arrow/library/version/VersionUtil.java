cf3a362007e7df4d8f90307e6c3b0f9199fa75bc
broken1arrow <broken2arrow1@gmail.com>
2026-04-10 15:51:03 +0200

    Improve the version logic for 26+

--- version/src/main/java/org/broken/arrow/library/version/VersionUtil.java
+++ version/src/main/java/org/broken/arrow/library/version/VersionUtil.java
@@ -9,6 +9,9 @@ import javax.annotation.Nullable;
 public class VersionUtil {
 
     private double version;
+    int major;
+    int minor;
+    int patch;
 
     /**
      * Constructs a ServerVersion instance by extracting the server version
@@ -36,7 +39,8 @@ public class VersionUtil {
      * @return true if the current server version is newer, false otherwise
      */
     public boolean versionNewer(double version) {
-        return version > this.version;
+        int[] v = split(version);
+        return compare(v[0], v[1]) > 0;
     }
 
     /**
@@ -47,7 +51,8 @@ public class VersionUtil {
      * @return true if the current server version is equal or newer, false otherwise
      */
     public boolean versionAtLeast(double version) {
-        return this.version >= version;
+        int[] v = split(version);
+        return compare(v[0], v[1]) >= 0;
     }
 
     /**
@@ -58,7 +63,7 @@ public class VersionUtil {
      * @return true if server version is between the numbers.
      */
     public boolean versionBetween(double min, double max) {
-        return min > this.version && max < this.version;
+        return versionAtLeast(min) && versionOlder(max);
     }
 
     /**
@@ -68,16 +73,28 @@ public class VersionUtil {
      * @return true if the current server version is older, false otherwise
      */
     public boolean versionOlder(double version) {
-        return version < this.version;
+        int[] v = split(version);
+        return compare(v[0], v[1]) < 0;
     }
 
     /**
      * Returns the parsed server version as a float.
      *
+     * @deprecated  should no longer be used.
      * @return the server version number
      */
+    @Deprecated
     public double getServerVersion() {
         return version;
+    }
+
+    /**
+     * Get the version for this Minecraft version
+     *
+     * @return a array of the version from major to patch.
+     */
+    public int[] getVersion() {
+        return new int[]{major, minor, patch};
     }
 
     /**
@@ -96,9 +113,26 @@ public class VersionUtil {
         final String firstNumber;
         String secondNumber;
         final String firstString = versionPieces[1];
+        final String mainVersionString = versionPieces[0];
+        int majorVersion = Integer.parseInt(mainVersionString);
+        if (majorVersion > 21) {
+            major = majorVersion;
+            minor = Integer.parseInt(firstString);
+            String patchString = versionPieces[2];
+            if (!patchString.isEmpty() && !Character.isDigit(patchString.charAt(0))) {
+                patchString = patchString.replaceAll("\\D.*", "");
+            }
+            try {
+                patch = Integer.parseInt(patchString);
+            } catch (NumberFormatException ignore) {
+                patch = 0;
+            }
+            version = Double.parseDouble(major + "." +  minor);
+            return;
+        }
         if (firstString.contains("-")) {
-            firstNumber = firstString.substring(0, firstString.lastIndexOf("-"));
-
+            int endIndex = firstString.lastIndexOf("-");
+            firstNumber = firstString.substring(0, Math.max(endIndex, 1));
             secondNumber = firstString.substring(firstString.lastIndexOf("-") + 1);
             final int index = secondNumber.toUpperCase().indexOf("R");
             if (index >= 0)
@@ -106,9 +140,25 @@ public class VersionUtil {
         } else {
             final String secondString = versionPieces[2];
             firstNumber = firstString;
-            secondNumber = secondString.substring(0, secondString.lastIndexOf("-"));
+            int endIndex = secondString.lastIndexOf("-");
+            secondNumber = secondString.substring(0, Math.max(endIndex, 1));
         }
         version = Double.parseDouble(firstNumber + "." + secondNumber);
+        major = 1;
+        minor = Integer.parseInt(firstNumber);
+        patch = Integer.parseInt(secondNumber);
     }
 
+    private int[] split(double value) {
+        int major = (int) value;
+        int minor = (int) Math.round((value - major) * 10);
+        return new int[]{major, minor};
+    }
+
+    private int compare(int major, int minor) {
+        if (this.major != major)
+            return Integer.compare(this.major, major);
+
+        return Integer.compare(this.minor, minor);
+    }
 }
