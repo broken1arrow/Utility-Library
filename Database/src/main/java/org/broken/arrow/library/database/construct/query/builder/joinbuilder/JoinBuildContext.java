f2ca8a7d3f7207183150ce8887fc5204b9d80e47
broken1arrow <broken2arrow1@gmail.com>
2026-07-22 22:42:49 +0200

    Remove a class not needed

--- Database/src/main/java/org/broken/arrow/library/database/construct/query/builder/joinbuilder/JoinBuildContext.java
+++ Database/src/main/java/org/broken/arrow/library/database/construct/query/builder/joinbuilder/JoinBuildContext.java
@@ -4,7 +4,6 @@ import org.broken.arrow.library.database.construct.query.QueryBuilder;
 import org.broken.arrow.library.database.construct.query.builder.comparison.ComparisonHandler;
 import org.broken.arrow.library.database.construct.query.columnbuilder.Aggregation;
 import org.broken.arrow.library.database.construct.query.columnbuilder.Column;
-import org.broken.arrow.library.database.construct.query.columnbuilder.refernces.ColumnRef;
 import org.broken.arrow.library.database.construct.query.columnbuilder.refernces.SqlArg;
 import org.broken.arrow.library.database.construct.query.utlity.Marker;
 
@@ -64,7 +63,7 @@ public class JoinBuildContext {
      * @param columnName the name of the column for the join condition
      * @return a {@link ComparisonHandler} to specify comparison operations
      */
-    public ComparisonHandler<JoinBuildContext> on(final String columnName, SqlArg arg) {
+    public ComparisonHandler<JoinBuildContext> on(final String columnName) {
         return this.on(columnName, a -> {
         });
     }
@@ -94,8 +93,6 @@ public class JoinBuildContext {
      * @return The join clause SQL fragment (including "on"), or empty string if none.
      */
     public String build() {
-        SqlArg.col("").value();
-        on("", SqlArg.col(""));
         if (conditionsList.isEmpty())
             return "";
         String condition = formatConditions(conditionsList);
